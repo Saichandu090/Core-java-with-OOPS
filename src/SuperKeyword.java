f69467a18a228fcfb821e510513885fbbf536840
Saichandu090 <marrisaichandu143@gmail.com>
2024-09-04 18:13:33 +0530

    Super keyword Ex added

--- src/SuperKeyword.java
+++ src/SuperKeyword.java
@@ -1,2 +1,34 @@
-public class SuperKeyword {
+public class SuperKeyword
+{
+    int age=21;
+    String myName="Chandu";
+    void me()
+    {
+        System.out.println("Chandu");
+    }
+
+    SuperKeyword(int age)
+    {
+        this.age=age;
+    }
+}
+
+class Main extends SuperKeyword
+{
+    int age;
+    String myName;
+    Main()
+    {
+        super(19);
+        System.out.println("Sub Class");
+        System.out.println(super.myName);
+    }
+
+    public static void main(String[] args) {
+
+        Main m=new Main();
+        System.out.println(m.age+","+m.myName);
+
+
+    }
 }
