6789a0f099b27729fe324c34532bc07711be5180
Saichandu090 <marrisaichandu143@gmail.com>
2024-09-04 17:12:04 +0530

    Object and Class Example Added

---
+++ src/ObjectAndClass.java
@@ -0,0 +1,10 @@
+public class ObjectAndClass
+{
+    String name;
+    public static void main(String[] args) {
+
+        ObjectAndClass o=new ObjectAndClass();
+        o.name="Chandu";
+        System.out.println(o.name);
+    }
+}
