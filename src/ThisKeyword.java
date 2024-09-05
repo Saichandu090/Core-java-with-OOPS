79696f21ac5e93932e94f64dcc89fe5170103d52
Saichandu090 <marrisaichandu143@gmail.com>
2024-09-05 09:10:41 +0530

    Updated This Keyword Uses

--- src/ThisKeyword.java
+++ src/ThisKeyword.java
@@ -1,17 +1,17 @@
-public class ThisKeyword
+class ThisKeyword
 {
     String company;
 
     ThisKeyword(String company)
     {
-        this.company=company;
+        this.company=company;  //Usage -1
     }
 
     ThisKeyword()
     {
-        this("BriskPe");
+        this("BriskPe");  //Usage-2
         System.out.println("Chaining");
-        this.call();
+        this.call(); //Usage-3
     }
 
     public void call()
@@ -24,11 +24,29 @@ public class ThisKeyword
         System.out.println(this.company);
     }
 
+    public ThisKeyword have(Object k)
+    {
+       // have(this); //Usage-4
+        return this; //Usage-5
+    }
 
+
+    class Anony
+    {
+       void print()
+       {
+           System.out.println("Hey "+ThisKeyword.this.company);
+           ThisKeyword.this.call();  //If its a non static member make use this keyword for not creating an Object.
+
+       }
+    }
     public static void main(String[] args) {
 
         ThisKeyword t=new ThisKeyword();
+        t.have(new ThisKeyword());
+
     }
+
 }
 
 //this keyword is used for initializing instance variables.
